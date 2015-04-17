ad285ec826307c135b31cdae8db35cc1eae054db
Jackson Dean <jldean2011@gmail.com>
2015-04-17 15:26:03 -0400

    adding templates for searchable time datatype

--- datatypes/searchabletime/searchabletimetype.php
+++ datatypes/searchabletime/searchabletimetype.php
@@ -1,7 +1,23 @@
 <?php
+/**
+ * File containing the searchableTimeType class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ * @version 2014.07.0
+ * @package kernel
+ */
+
 
 class searchableTimeType extends eZTimeType
 {
+    const DATA_TYPE_STRING = "searchabletime";
+
+    function searchableTimeType()
+    {
+        $this->eZDataType( self::DATA_TYPE_STRING, ezpI18n::tr( 'kernel/classes/datatypes', "Searchable Time", 'Datatype name' ),
+                           array( 'serialize_supported' => true ) );
+    }
 
     function isIndexable()
     {
